bc422bf756cff2b001b10f4957a81c0054e846b4
Arie <arie@wego.com>
2013-04-27 11:31:56 +0700

    update

--- py/min.server.py
+++ py/min.server.py
@@ -1,6 +1,8 @@
 import serial
 from time import sleep
 ser = serial.Serial('/dev/ttyACM0', 9600)
-while 1 :
+while 1:
+    ser.write('1')
+    sleep(0.02)
     ser.readline()
-    sleep(0.5)
+    sleep(0.08)
