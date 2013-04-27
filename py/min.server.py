f44a1831a45107da9f3160ac5ef935ddb1afa237
Arie <arie@wego.com>
2013-04-27 11:19:44 +0700

    update

--- py/min.server.py
+++ py/min.server.py
@@ -1,8 +1,18 @@
 import serial
-import time
-## check serial port
-def checkokay():
-ser.flushInput()
-time.sleep(3)
-line=ser.readline()
-time.sleep(3)
+from sys import exit
+import os
+ser = serial.Serial('/dev/ttyACM0', 9600)
+#Setting first boot temperature 
+set_temp = 0
+while True:
+        
+        if set_temp == 0:
+                set_temp = raw_input("What temperature would you like? ")
+        else:
+                 print "Menu:"       
+        
+        print "Hello what would you like to check?"
+        print "1. Temperature"
+        print "2. Humidity"
+        print "3. Settings"
+        print "4. Exit"
